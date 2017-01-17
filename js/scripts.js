3f723771e1045a0d549abc3f3322d654b4281f19
Chad Durkin and Sam Kirsch <sam@>
2017-01-17 13:27:38 -0800

    add function to insert image elements from the randomly chosen array into the html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var deckArray = [1,2,3,4,5,6];
+var deckArray = [[1, 2],[2,3],[3,4],[4,5],[5,6],[6,7]];
 var readingArray = [];
 
 var generateReading = function(readingArray) {
@@ -12,13 +12,33 @@ var generateReading = function(readingArray) {
   }
 };
 
+var addImg = function(imageLink) {
+  var divInsert = document.createElement('div');
+  divInsert.className = 'card-image-holder';
+
+  divInsert.innerHTML = imageLink;
+
+  document.getElementById('test').appendChild(divInsert);
+}
+
 var deal = function(readingArray, deckArray) {
   // links the cards dealt to positions on the page
+  var cardLocationID = 1;
+  readingArray.forEach(function(cards){1
+    var image = cards[0];
+    var imageLink = '<img src="img/' + image + '.png" />';
+    addImg(imageLink);
+    console.log(imageLink);
+    console.log("card ID is " + cardLocationID);
+    cardLocationID++;
+  })
 };
 
 $(document).ready(function() {
   generateReading(readingArray);
   console.log(readingArray);
-  generateReading(readingArray);
-  console.log(readingArray);
+  deal(readingArray, deckArray);
 });
+
+
+// $("#test").parseHTML(imageLink);
